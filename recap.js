5ddfc77425d644e7aa79d3e657d4363d3f4ba382
Iftykhar <s.m.ifty49@gmail.com>
2021-08-29 04:20:05 +0600

    practice

---
+++ recap.js
@@ -0,0 +1,19 @@
+// 1. let and const 
+const hubby = 'omor sani';
+let phone = 'iphone';
+phone = 'android';
+
+//back tick
+const myNotes = `i am a ${hubby}. and i don't have a ${phone}`;
+console.log(myNotes);
+
+// default parameter 
+function maxNumber(array = []) {
+    const max = Math.max(...array); //spread operator
+    return max;
+}
+
+const number = [20, 89, 13, 78];
+console.log(maxNumber(number));
+
+//arrow
